d72479240b429b449ab2122b1bd927ffd59a5ff2
Kent Kostelac <14237820+kentkost@users.noreply.github.com>
2023-08-31 02:19:23 +0200

    added method for decoding between arbitrary types

--- src/readers/tap3reader.c
+++ src/readers/tap3reader.c
@@ -5,28 +5,22 @@
 #include <asn_application.h>
 #include <asn_internal.h>    /* for ASN__DEFAULT_STACK_MAX */
 
-typedef asn_dec_rval_t (*pfn_decoder)(const asn_codec_ctx_t *opt_codec_ctx,const asn_TYPE_descriptor_t *type_descriptor, void **struct_ptr,const void *ptr, size_t size) ;
-
 typedef struct {
     const char *name;
     enum asn_transfer_syntax syntax;
-    pfn_decoder func;
-    const char *full_name;
 } syntax_selector;
 
-void decode_ber_datainterchange(char *path);
-void generic_buffer_decode_datainterchange(char *path);
 static void tap0311_menu();
-static void * get_decoder(int x);
-const char* decode_tap0311(int inputSelector, int outputSelector, char *inputBytes);
+extern char* decode_tap0311_datainterchange(int inputSelector, int outputSelector, char *inputBytes);
 
-static const syntax_selector input_encoders[] = {
-    {
-        "ber", 
-        ATS_BER, 
-        ber_decode,
-        "input is BER or DER"
-    },
+static const syntax_selector encodings[] = {
+    [BER]   = {"BER", ATS_BER},
+    [DER]   = {"DER", ATS_DER},
+    [XER]   = {"XER", ATS_BASIC_XER},
+    [JER]   = {"JER", ATS_JER}, // not implemented for decode yet.
+    [OER]   = {"OER", ATS_BASIC_OER},
+    [PER]   = {"PER", ATS_ALIGNED_BASIC_PER},
+    [UPER]  = {"UPER", ATS_UNALIGNED_BASIC_PER},
 };
 
 int main(int argc, char** argv)
@@ -47,118 +41,71 @@ int main(int argc, char** argv)
 }
 
 static void tap0311_menu(){
-    decode_ber_datainterchange("E:\\repos\\tap3reader\\sample-data\\tap3-sample-DataInterChange-3_11.ber");
+    decode_tap0311_datainterchange(0,2,"E:\\repos\\tap3reader\\sample-data\\tap3-sample-DataInterChange-3_11.ber");
     // decode_tap0311(0,1,"ree");
 }
 
-
-const char* decode_tap0311(int inputSelector, int outputSelector, char *inputBytes)
-{
-
+/* Dump the buffer out to the specified FILE */
+static int write_out(const void *buffer, size_t size, void *key) {
+    FILE *fp = (FILE *)key;
+    return (fwrite(buffer, 1, size, fp) == size) ? 0 : -1;
 }
 
-static void * get_decoder(int inputFormat)
+extern char* decode_tap0311_datainterchange(int input_selector, int output_selector, char *file_path)
 {
+    // function to validate selection.
+    syntax_selector input_sel = encodings[input_selector];
+    syntax_selector output_sel = encodings[output_selector];
 
-
-}
-
-void decode_ber_datainterchange(char *path)
-{
-    asn_dec_rval_t rval; /* Decoder return value */
+    asn_dec_rval_t decode_result; /* Decoder return value */
     DataInterChange_t *datainterchange = 0; /* Type to decode. Note this 0! */
     FILE *fp; /* Input file handler */
     size_t size; /* Number of bytes read */
-    // char *filename="E:\\repos\\tap3reader\\sample-data\\tap3-sample-DataInterChange-3_11.ber"; /* Input file name */
-    char *filename="E:\\repos\\tap3reader\\build\\debug-readers\\tap3-sample-DataInterChange-3_11.ber"; /* Input file name */
-
-    /* Open input file as read-only binary */
-    fp = fopen(filename, "rb");
-    if(!fp) {
-        perror(filename);
-        exit(1);
-    }
-
-    fseek(fp, 0L, SEEK_END);
-    int buffer_size = ftell(fp);
-    rewind(fp);
     
-    char buf[buffer_size];
-    /* Read the entire file */
-    size = fread(buf, 1, sizeof(buf), fp);
-
-    fclose(fp);
-    if(!size) {
-        fprintf(stderr, "%s: Empty or broken\n", filename);
-        exit(1);
-    }
-
-    /* Decode the input buffer as circle type */
-    syntax_selector sel = input_encoders[0];
-    rval = sel.func(0, &asn_DEF_DataInterChange, (void **)&datainterchange, buf, size); // using function pointer
-    // rval = ber_decode(0, &asn_DEF_DataInterChange, (void **)&datainterchange, buf, size); // direct reference
-
-    if(rval.code != RC_OK) {
-        fprintf(stderr, "%s: Broken datainterchange encoding at byte %ld\n", filename, (long)rval.consumed);
-        exit(1);
-    }
-
-    /* Print the decoded circle type as XML */
-    // xer_fprint(stdout, &asn_DEF_DataInterChange, datainterchange);
-    jer_fprint(stdout, &asn_DEF_DataInterChange, datainterchange);
-}
-
-void generic_buffer_decode_datainterchange(char *path)
-{
-    asn_dec_rval_t rval; /* Decoder return value */
-    DataInterChange_t *datainterchange = 0; /* Type to decode. Note this 0! */
-    FILE *fp; /* Input file handler */
-    size_t size; /* Number of bytes read */
-    // char *filename="E:\\repos\\tap3reader\\sample-data\\tap3-sample-DataInterChange-3_11.ber"; /* Input file name */
-    char *filename="E:\\repos\\tap3reader\\build\\debug-readers\\tap3-sample-DataInterChange-3_11.ber"; /* Input file name */
+    char *path="E:\\repos\\tap3reader\\build\\debug-readers\\tap3-sample-DataInterChange-3_11.ber"; /* Input file name */
+    // char *path = file_path;
 
     /* Open input file as read-only binary */
-    fp = fopen(filename, "rb");
+    fp = fopen(path, "rb");
     if(!fp) {
-        perror(filename);
+        perror(path);
         exit(1);
     }
-
+    
     fseek(fp, 0L, SEEK_END);
     int buffer_size = ftell(fp);
     rewind(fp);
     
     // set the size of the byte array to some arbitrary length. buffer_size is enough.
     static uint8_t *buf;
-    buf = (uint8_t *)REALLOC(buf, 8192);
-    /* Read up to the buffer size */
-    // size = fread(buf, 1, sizeof(buf), fp);
+    buf = (uint8_t *)REALLOC(buf, buffer_size);
     size = fread(buf, 1, buffer_size, fp);
 
     fclose(fp);
     if(!size) {
-        fprintf(stderr, "%s: Empty or broken\n", filename);
+        fprintf(stderr, "%s: Empty or broken\n", path);
         exit(1);
     }
 
-    // Test code
+
     asn_codec_ctx_t *opt_codec_ctx = 0;
-    enum asn_transfer_syntax isyntax = ATS_BER;
+    enum asn_transfer_syntax isyntax = input_sel.syntax;
     asn_TYPE_descriptor_t *pduType = &asn_DEF_DataInterChange;
-    static uint8_t *fbuf = 0;
     uint8_t *i_bptr = buf; //the next place to read from. The idea is that you update this constantly as you buffer through the file.
     size_t   i_size = size;
     
     /* Decode the input buffer as circle type */
-    rval = asn_decode(opt_codec_ctx, isyntax, pduType, (void **)&datainterchange, i_bptr, i_size); //using asn_application.h
+    decode_result = asn_decode(opt_codec_ctx, isyntax, pduType, (void **)&datainterchange, i_bptr, i_size); //using asn_application.h
 
-    if(rval.code != RC_OK) {
-        fprintf(stderr, "%s: Broken datainterchange encoding at byte %ld\n", filename, (long)rval.consumed);
+    if(decode_result.code != RC_OK) {
+        fprintf(stderr, "%s: Broken datainterchange encoding at byte %ld\n", path, (long)decode_result.consumed);
         exit(1);
     }
 
     /* Print the decoded circle type as XML */
     // xer_fprint(stdout, &asn_DEF_DataInterChange, datainterchange);
-    jer_fprint(stdout, &asn_DEF_DataInterChange, datainterchange);
+    enum asn_transfer_syntax osyntax = output_sel.syntax;
+    asn_enc_rval_t encode_res = asn_encode(NULL, osyntax, pduType, datainterchange, write_out, stdout);
+    
+    free(buf);
 }
-
--- src/readers/tap3reader.h
+++ src/readers/tap3reader.h
@@ -1,7 +1,7 @@
 #ifndef __TAP3READER_H__
 #define __TAP3R3EADER_H__
 
-enum codec{BER=0, DER, XER, JER, OER, UPER, PER};
+enum codec{BER=0, DER, XER, OER, UPER, PER, JER};
 enum version{TAP0301=1, TAP0302=2,TAP0303=3,TAP0304=4,TAP0309=9,TAP0310=10,TAP0311=11,TAP0312=12};
 
 const char * const codec_str[] =
