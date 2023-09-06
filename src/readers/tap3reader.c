6232389972fbd9c78fdad38b9609521fb80239d5
Kent Kostelac <14237820+kentkost@users.noreply.github.com>
2023-09-07 01:04:28 +0200

    outlined how I want the functions

--- src/readers/tap3reader.c
+++ src/readers/tap3reader.c
@@ -14,6 +14,17 @@ typedef struct {
 static void tap0311_menu();
 extern char* decode_tap0311_datainterchange(int inputSelector, int outputSelector, char *file_path);
 
+// Decoders
+static char* decode_datainterchange_buffer(int input_selector, char *file_path);
+static DataInterChange_t* decode_datainterchange_file(int input_selector, char *file_path);
+// Encoders
+static char* encode_datainterchange_2buffer(DataInterChange_t* datainterchange, int output_selector);
+static char* encode_datainterchange_2file(DataInterChange_t* datainterchange, int output_selector, char* filename);
+
+extern char* decode_tap0311_datainterchange_file2file(int input_selector, int output_selector, char *file_path, char *newfileName);
+extern char* decode_tap0311_datainterchange_buffer2file(int input_selector, int output_selector, char *in_buffer, char *newFileName);
+extern char* decode_tap0311_datainterchange_buffer2buffer(int input_selector, int output_selector, char *in_buffer, char *out_buffer);
+
 static const syntax_selector encodings[] = {
     [BER]   = {"BER", ATS_BER},
     [DER]   = {"DER", ATS_DER},
@@ -27,28 +38,94 @@ static const syntax_selector encodings[] = {
 int main(int argc, char** argv)
 {
     tap0311_menu();
-    return 0;
+    return 1;
 }
 
 static void tap0311_menu(){
-    decode_tap0311_datainterchange(0,2,"E:\\repos\\tap3magic\\sample-data\\tap3-sample-DataInterChange-3_11.ber");
+
+    DataInterChange_t *ree = decode_datainterchange_file(0, "reee");
+    // decode_tap0311_datainterchange(0,2,"E:\\repos\\tap3magic\\sample-data\\tap3-sample-DataInterChange-3_11.ber");
     // decode_tap0311(0,1,"ree");
 }
 
-// char *global_buf;
-// int offset = 0;
-// /* Dump the buffer out to the specified FILE */
-// static int write_out(const void *buffer, size_t size, void *key) {
-//     FILE *fp = (FILE *)key;
+static DataInterChange_t* decode_datainterchange_file(int input_selector, char *file_path){
+        // function to validate selection.
+    syntax_selector input_sel = encodings[input_selector];
+    asn_dec_rval_t decode_result; /* Decoder return value */
+    DataInterChange_t *datainterchange = 0; /* Type to decode. Note this 0! */
+    FILE *fp; /* Input file handler */
+    size_t size; /* Number of bytes read */
     
-//     memcpy(global_buf+offset, buffer, size);
-//     int res = fwrite(buffer, 1, size, fp) == size ? 0 : -1;
-//     offset+=size;
-    
-//     return res;
-// }
+    char *path="E:\\repos\\tap3magic\\build\\debug-readers\\tap3-sample-DataInterChange-3_11.ber"; /* Input file name */
+    // char *path = file_path;
 
-extern char* decode_tap0311_datainterchange(int input_selector, int output_selector, char *file_path)
+    /* Open input file as read-only binary */
+    fp = fopen(path, "rb");
+    if(!fp) {
+        perror(path);
+        exit(1);
+    }
+    
+    fseek(fp, 0L, SEEK_END);
+    int buffer_size = ftell(fp);
+    rewind(fp);
+    
+    // set the size of the byte array to some arbitrary length. buffer_size is enough.
+    static uint8_t *buf;
+    buf = (uint8_t *)REALLOC(buf, buffer_size);
+    size = fread(buf, 1, buffer_size, fp);
+
+    fclose(fp);
+    if(!size) {
+        fprintf(stderr, "%s: Empty or broken\n", path);
+        exit(1);
+    }
+
+
+    asn_codec_ctx_t *opt_codec_ctx = 0;
+    enum asn_transfer_syntax isyntax = input_sel.syntax;
+    asn_TYPE_descriptor_t *pduType = &asn_DEF_DataInterChange;
+    uint8_t *i_bptr = buf; //the next place to read from. The idea is that you update this constantly as you buffer through the file.
+    size_t   i_size = size;
+    
+    /* Decode the input buffer as circle type */
+    decode_result = asn_decode(opt_codec_ctx, isyntax, pduType, (void **)&datainterchange, i_bptr, i_size); //using asn_application.h
+
+    if(decode_result.code != RC_OK) {
+        fprintf(stderr, "%s: Broken datainterchange encoding at byte %ld\n", path, (long)decode_result.consumed);
+        exit(1);
+    }
+
+    return datainterchange;
+}
+
+
+static char* decode_datainterchange_buffer(int input_selector, char *file_path){
+    return 0;
+}
+
+static char* encode_datainterchange_2buffer(DataInterChange_t* datainterchange, int output_selector){
+
+}
+
+static char* encode_datainterchange_2file(DataInterChange_t* datainterchange, int output_selector, char* filename){
+    
+}
+
+extern char* decode_tap0311_datainterchange_file2file(int input_selector, int output_selector, char *file_path, char *newfileName){
+    return 0;
+}
+
+extern char* decode_tap0311_datainterchange_buffer2file(int input_selector, int output_selector, char *in_buffer, char *newFileName){
+    return 0;
+}
+
+extern char* decode_tap0311_datainterchange_buffer2buffer(int input_selector, int output_selector, char *in_buffer, char *out_buffer){
+    return 0;
+}
+
+
+extern char* decode_tap0311_datainterchange_file2buffer(int input_selector, int output_selector, char *file_path)
 {
     // function to validate selection.
     syntax_selector input_sel = encodings[input_selector];
@@ -127,4 +204,5 @@ extern char* decode_tap0311_datainterchange(int input_selector, int output_selec
     printf("%s", ree);
     // free(buf);
     free(ree);
+    return ree;
 }
